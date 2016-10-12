39ddbe300c318449f0a31c2f3c0763413d421adc
echoflame14 <joshpeterson.dev@gmail.com>
2016-10-11 22:23:15 -0600

    added comments for future

--- frontend/angular/ctrl.js
+++ frontend/angular/ctrl.js
@@ -1,45 +1,42 @@
 angular.module("app").controller("ctrl",function($scope, $timeout, srvc){
-	$scope.test = srvc.test;
-	$scope.min = 25;
-	$scope.sec = "00";
-	var running = false;
-	$scope.action = "START";
-	var timer = function(){
-		console.log(running);
-		if($scope.min === 0 && $scope.sec === 1){
+	$scope.min = 25; // setting the initial minute count
+	$scope.sec = "00"; // setting the initial second count to "00" to display properly
+	var running = false; // setting the initial state of running to be false
+	$scope.action = "START"; // text for the action button
+	var timer = function(){ // func will do the countdown
+		if($scope.min === 0 && $scope.sec === 1){ // checking for the timer to be done to stop the $timeout func
 			$scope.min = "00";
-			$scope.sec = "00" ;
+			$scope.sec = "00";
+			alert("CONGRATS you finished a 25 min block")
 		}
-		else if($scope.sec === 1 && $scope.min !== 0){
+		else if($scope.sec === 1 && $scope.min !== 0){ // decrements the min and starts the timer func again
 			$scope.min--;
 			$scope.sec = 60;
 			$timeout(timer,1000);
 		}
-		else if(running === true){
+		else if(running){ // if running is true countdown the sec and start the func again using $timeout
 			$scope.sec--;
 			$timeout(timer,1000);
 		}
 	}
-	$(".startBtn").click(function(){
-		if(!running){
+	$(".startBtn").click(function(){ // logic for the action button press
+		if(!running){ // if not counting down then start the timer
 			$timeout(timer,500);
-			if($scope.savedMin && $scope.savedSec){
+			if($scope.savedMin && $scope.savedSec){ // setting the min and sec using the saved val's from when it was paused
 				$scope.min = $scope.savedMin;
 				$scope.sec = $scope.savedSec;
-			}else{
+			}else{ // if it was not paused then set the min and sec val's to default
 				$scope.sec = 60;
 				$scope.min = 24;
 			}
-			$scope.action = "PAUSE";
-			running = true;
-			console.log(running);
+			$scope.action = "PAUSE"; // setting the text to pause
+			running = true; // running set to true
 		}
-		else{
+		else{ // if the timer is counting down then this will run (saving the values of min an sec to be used when the timer is started again)
 			$scope.savedMin = $scope.min;
 			$scope.savedSec = $scope.sec;
 			running = false;
-			$scope.action = "START";
-			console.log(running);
+			$scope.action = "START"; // changing the text for the action button
 		}
 	});
 });
--- frontend/angular/srvc.js
+++ frontend/angular/srvc.js
@@ -1,29 +1,4 @@
 angular.module("app").service("srvc", function($http){
 
-	this.timer = function(min,sec){
-		console.log(min,":",sec);
-		var min = min;
-		var sec = sec;
-		if(sec === 1){
-			sec = 60;
-			min = min -1;
-		}
-		else{
-			sec--;
-		}
-		return [min,sec];
-	}
 });
-
-
-// Sudo code for the timer function
-// needs a countdown function to assign this.sec every time it runs
-// it should only run every 1 sec
-
-// sec passed in as 0
-// min passed in as 25
-
-// setTimeout(function(){ alert("Hello"); }, 3000);
-
-//need to set the timer to end if min and sec are both 0
-//else setTimeout needs to run again
+// currently not using the service
