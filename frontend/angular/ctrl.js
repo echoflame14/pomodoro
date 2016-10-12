41f4b8fb4dd08cdff819cd7803b7123a5fc422a1
echoflame14 <joshpeterson.dev@gmail.com>
2016-10-11 22:01:33 -0600

    pause feature now works

--- frontend/angular/ctrl.js
+++ frontend/angular/ctrl.js
@@ -2,7 +2,10 @@ angular.module("app").controller("ctrl",function($scope, $timeout, srvc){
 	$scope.test = srvc.test;
 	$scope.min = 25;
 	$scope.sec = "00";
+	var running = false;
+	$scope.action = "START";
 	var timer = function(){
+		console.log(running);
 		if($scope.min === 0 && $scope.sec === 1){
 			$scope.min = "00";
 			$scope.sec = "00" ;
@@ -12,14 +15,31 @@ angular.module("app").controller("ctrl",function($scope, $timeout, srvc){
 			$scope.sec = 60;
 			$timeout(timer,1000);
 		}
-		else{
+		else if(running === true){
 			$scope.sec--;
 			$timeout(timer,1000);
 		}
 	}
 	$(".startBtn").click(function(){
-		$timeout(timer,500);
-		$scope.sec = 60;
-		$scope.min = 24;
+		if(!running){
+			$timeout(timer,500);
+			if($scope.savedMin && $scope.savedSec){
+				$scope.min = $scope.savedMin;
+				$scope.sec = $scope.savedSec;
+			}else{
+				$scope.sec = 60;
+				$scope.min = 24;
+			}
+			$scope.action = "PAUSE";
+			running = true;
+			console.log(running);
+		}
+		else{
+			$scope.savedMin = $scope.min;
+			$scope.savedSec = $scope.sec;
+			running = false;
+			$scope.action = "START";
+			console.log(running);
+		}
 	});
 });
